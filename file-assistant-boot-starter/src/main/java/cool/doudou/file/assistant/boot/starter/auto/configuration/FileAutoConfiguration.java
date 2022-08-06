75fdb884fdf6aa39bc929c424713f3dd8ae01240
这么诚实 <jiangchengshi@gmail.com>
2022-08-07 00:25:31 +0800

    feat: 1.0.8 (spring-boot 2.6.10)

--- file-assistant-boot-starter/src/main/java/cool/doudou/file/assistant/boot/starter/auto/configuration/FileAutoConfiguration.java
+++ file-assistant-boot-starter/src/main/java/cool/doudou/file/assistant/boot/starter/auto/configuration/FileAutoConfiguration.java
@@ -7,12 +7,10 @@ import cool.doudou.file.assistant.core.enums.StorageModeEnum;
 import cool.doudou.file.assistant.core.helper.*;
 import cool.doudou.file.assistant.core.properties.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -23,7 +21,7 @@ import org.springframework.context.annotation.Import;
  */
 @EnableConfigurationProperties({FileProperties.class, GridFsProperties.class, AliYunProperties.class, MinIoProperties.class, LocalProperties.class})
 @Import({GridFsConfig.class, AliYunConfig.class, MinIOConfig.class})
-@AutoConfiguration(before = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@Configuration
 public class FileAutoConfiguration {
     private FileProperties fileProperties;
 
